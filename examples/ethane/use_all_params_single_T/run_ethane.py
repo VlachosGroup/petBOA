033aeda3ed9ef9ce1d643607e3ad3aabfcf37a7e
Sashank Kasiraju <skasiraj@udel.edu>
2021-05-13 00:32:36 -0400

    Rename and reorg for folder cleanup

--- examples/ethane/use_all_params_single_T/run_ethane.py
+++ examples/ethane/use_all_params_single_T/run_ethane.py
@@ -4,7 +4,7 @@ Test on ethane system ODEs
 import os
 import sys
 
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 sys.path.insert(0, project_path)
 
 import time
@@ -17,8 +17,9 @@ from estimator.optimizer import ModelBridge, BOOptimizer
 
 import matplotlib.pyplot as plt
 import matplotlib
+
 # Set matplotlib default values
-font = {'size'   : 20}
+font = {'size': 20}
 
 matplotlib.rc('font', **font)
 matplotlib.rcParams['axes.linewidth'] = 1.5
@@ -32,26 +33,26 @@ def plot_ethane(t_vec, C_profile, title=None, save_path=None):
     """
     plot the ode profiles 
     """
-    fig, ax = plt.subplots(figsize=(6,6))
-    ax.plot(t_vec, C_profile[:,0], label = r'$\rm C_{2}H_{6}$')
-    ax.plot(t_vec, C_profile[:,1], label = r'$\rm C_{2}H_{4}$')
-    ax.plot(t_vec, C_profile[:,2], label = r'$\rm CH_{4}$')
-    ax.plot(t_vec, C_profile[:,3], label = r'$\rm H_{2}$')
-    ax.plot(t_vec, C_profile[:,4], label = r'$\rm CO_{2}$')
-    ax.plot(t_vec, C_profile[:,5], label = r'$\rm CO$')
-    ax.plot(t_vec, C_profile[:,6], label = r'$\rm H_{2}O$')
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(t_vec, C_profile[:, 0], label=r'$\rm C_{2}H_{6}$')
+    ax.plot(t_vec, C_profile[:, 1], label=r'$\rm C_{2}H_{4}$')
+    ax.plot(t_vec, C_profile[:, 2], label=r'$\rm CH_{4}$')
+    ax.plot(t_vec, C_profile[:, 3], label=r'$\rm H_{2}$')
+    ax.plot(t_vec, C_profile[:, 4], label=r'$\rm CO_{2}$')
+    ax.plot(t_vec, C_profile[:, 5], label=r'$\rm CO$')
+    ax.plot(t_vec, C_profile[:, 6], label=r'$\rm H_{2}O$')
     ax.set_xlabel('t (s)')
     ax.set_ylabel('C (mol/L)')
     ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    
+
     fig_name = 'profile'
     if title is not None:
         ax.set_title(title)
         fig_name += ('_' + title.replace(' ', ''))
-    if save_path is None: 
+    if save_path is None:
         save_path = os.getcwd()
-        
-    fig.savefig(os.path.join(save_path, fig_name+'.png'), bbox_inches="tight")
+
+    fig.savefig(os.path.join(save_path, fig_name + '.png'), bbox_inches="tight")
 
 
 def plot_ethane_overlap(t_vec1, C_profile1, t_vec2, C_profile2, title=None, save_path=None):
@@ -59,37 +60,38 @@ def plot_ethane_overlap(t_vec1, C_profile1, t_vec2, C_profile2, title=None, save
     Plot the ode profiles, 
     Check whether the first profile matches with the second
     """
-    fig, ax = plt.subplots(figsize=(6,6))
-    ax.plot(t_vec1, C_profile1[:,0], label = r'$\rm C_{2}H_{6}$')
-    ax.plot(t_vec1, C_profile1[:,1], label = r'$\rm C_{2}H_{4}$')
-    ax.plot(t_vec1, C_profile1[:,2], label = r'$\rm CH_{4}$')
-    ax.plot(t_vec1, C_profile1[:,3], label = r'$\rm H_{2}$')
-    ax.plot(t_vec1, C_profile1[:,4], label = r'$\rm CO_{2}$')
-    ax.plot(t_vec1, C_profile1[:,5], label = r'$\rm CO$')
-    ax.plot(t_vec1, C_profile1[:,6], label = r'$\rm H_{2}O$')
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(t_vec1, C_profile1[:, 0], label=r'$\rm C_{2}H_{6}$')
+    ax.plot(t_vec1, C_profile1[:, 1], label=r'$\rm C_{2}H_{4}$')
+    ax.plot(t_vec1, C_profile1[:, 2], label=r'$\rm CH_{4}$')
+    ax.plot(t_vec1, C_profile1[:, 3], label=r'$\rm H_{2}$')
+    ax.plot(t_vec1, C_profile1[:, 4], label=r'$\rm CO_{2}$')
+    ax.plot(t_vec1, C_profile1[:, 5], label=r'$\rm CO$')
+    ax.plot(t_vec1, C_profile1[:, 6], label=r'$\rm H_{2}O$')
     # scatter for the second profile
-    ax.scatter(t_vec2, C_profile2[:,0], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,1], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,2], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,3], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,4], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,5], s= 5, alpha = 0.3)
-    ax.scatter(t_vec2, C_profile2[:,6], s= 5, alpha = 0.3)
-    
+    ax.scatter(t_vec2, C_profile2[:, 0], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 1], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 2], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 3], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 4], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 5], s=5, alpha=0.3)
+    ax.scatter(t_vec2, C_profile2[:, 6], s=5, alpha=0.3)
+
     ax.set_xlabel('t (s)')
     ax.set_ylabel('C (mol/L)')
     ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    
+
     fig_name = 'profiles_overlap'
     if title is not None:
         ax.set_title(title)
         fig_name += ('_' + title.replace(' ', ''))
-    if save_path is None: 
+    if save_path is None:
         save_path = os.getcwd()
-        
-    fig.savefig(os.path.join(save_path, fig_name+'.png'), bbox_inches="tight")
-        
-#%% Define the problem 
+
+    fig.savefig(os.path.join(save_path, fig_name + '.png'), bbox_inches="tight")
+
+
+# %% Define the problem
 # Reaction equations:
 # EDH: ethane dehydrogenation: C2H6 -> C2H4 + H2
 # Hyd: hydrogenolysis: C2H6 + H2 -> 2CH4
@@ -97,7 +99,7 @@ def plot_ethane_overlap(t_vec1, C_profile1, t_vec2, C_profile2, title=None, save
 
 # stoichiometry matrix
 # C2H6, C2H4, CH4, H2, CO2, CO, H2O
-stoichiometry = [[-1, 1, 0, 1, 0, 0, 0], 
+stoichiometry = [[-1, 1, 0, 1, 0, 0, 0],
                  [-1, 0, 2, -1, 0, 0, 0],
                  [0, 0, 0, -1, -1, 1, 1]]
 
@@ -111,7 +113,7 @@ m_specs = 7
 energy_unit = 'kJ'
 R_unit = 'kJ/mol/K'
 R_bar_unit = 'L bar/mol/K'
-temperature = 873 #K
+temperature = 873  # K
 
 # Inlet Concentration Vals in M
 C2H6_in = 0.0005
@@ -123,7 +125,6 @@ CO_in = 0
 H2O_in = 0
 C0 = [C2H6_in, C2H4_in, CH4_in, H2_in, CO2_in, CO_in, H2O_in]
 
-
 # Integration time
 tf = 10
 
@@ -133,11 +134,11 @@ t_eval = np.linspace(0, tf, n_int)
 
 # Set ground truth parameter values
 A0_EDH = 2.5E6
-Ea_EDH = 125 #kJ/mol
+Ea_EDH = 125  # kJ/mol
 A0_Hyd = 3.8E8
-Ea_Hyd = 110 #kJ/mol
+Ea_Hyd = 110  # kJ/mol
 A0_RWGS = 1.9E6
-Ea_RWGS = 70 #kJ/mol
+Ea_RWGS = 70  # kJ/mol
 
 # Kp values calculated at different T 
 # T kp value pair in a dictionary
@@ -156,14 +157,14 @@ Kp_RWGS = {835: 0.296,
 
 # Ground truth parameter
 para_ethane = {'EDH_prefactor': np.log10(A0_EDH),
-                'EDH_Ea': Ea_EDH,
-                'EDH_eq': np.log10(Kp_EDH[temperature]),
-                'Hyd_prefactor': np.log10(A0_Hyd),
-                'Hyd_Ea': Ea_Hyd,
-                'Hyd_eq': np.log10(Kp_Hyd[temperature]),
-                'RWGS_prefactor': np.log10(A0_RWGS),
-                'RWGS_Ea': Ea_RWGS,
-                'RWGS_eq': np.log10(Kp_RWGS[temperature])}
+               'EDH_Ea': Ea_EDH,
+               'EDH_eq': np.log10(Kp_EDH[temperature]),
+               'Hyd_prefactor': np.log10(A0_Hyd),
+               'Hyd_Ea': Ea_Hyd,
+               'Hyd_eq': np.log10(Kp_Hyd[temperature]),
+               'RWGS_prefactor': np.log10(A0_RWGS),
+               'RWGS_Ea': Ea_RWGS,
+               'RWGS_eq': np.log10(Kp_RWGS[temperature])}
 
 # parameter names
 para_name_ethane = list(para_ethane.keys())
@@ -175,6 +176,7 @@ para_ground_truth = list(para_ethane.values())
 estimator_name = 'ethane'
 ut.clear_cache(estimator_name)
 
+
 def rate_eq(concentrations, para_dict, stoichiometry, name, temperature):
     """
     Rate equation involve prefactor, Ea and 
@@ -183,46 +185,45 @@ def rate_eq(concentrations, para_dict, stoichiometry, name, temperature):
     # Convert to nparray
     concentrations = np.array(concentrations, dtype=float)
     stoichiometry = np.array(stoichiometry)
-    
+
     # Get the constants
     R_bar = c.R(R_bar_unit)
     R = c.R(R_unit)
-    
+
     # Extract the parameters
-    prefactor = 10**para_dict[name+'_prefactor']
-    Ea = para_dict[name+'_Ea']
-    KEQ = 10**para_dict[name+'_eq']
-    
+    prefactor = 10 ** para_dict[name + '_prefactor']
+    Ea = para_dict[name + '_Ea']
+    KEQ = 10 ** para_dict[name + '_eq']
+
     # Reaction quotient 
     Qp = 0
     # Find the concentrations of the reactants
     reactant_index = np.where(stoichiometry < 0)[0]
     participant_index = np.where(stoichiometry != 0)[0]
-    C_reactant_prod = np.prod(np.power(concentrations[reactant_index], - stoichiometry[reactant_index])) 
-    
+    C_reactant_prod = np.prod(np.power(concentrations[reactant_index], - stoichiometry[reactant_index]))
+
     if C_reactant_prod != 0:
-        Qp = np.prod(np.power(concentrations[participant_index], stoichiometry[participant_index]))\
-            * np.power(temperature * R_bar, np.sum(stoichiometry)) 
-    
-    rate_value = prefactor * np.exp(-Ea/R/temperature) * C_reactant_prod * (1 - Qp/KEQ)
-        
+        Qp = np.prod(np.power(concentrations[participant_index], stoichiometry[participant_index])) \
+             * np.power(temperature * R_bar, np.sum(stoichiometry))
+
+    rate_value = prefactor * np.exp(-Ea / R / temperature) * C_reactant_prod * (1 - Qp / KEQ)
+
     return rate_value
 
-#%% Tests on the ethane system, with no noise
+
+# %% Tests on the ethane system, with no noise
 # Test on whether the rate can be calculated correctly
-rate_EDH = rate_eq(C0, para_ethane, stoichiometry[0],  'EDH', temperature)
+rate_EDH = rate_eq(C0, para_ethane, stoichiometry[0], 'EDH', temperature)
 rate_Hyd = rate_eq(C0, para_ethane, stoichiometry[1], 'Hyd', temperature)
-rate_RWGS = rate_eq(C0, para_ethane, stoichiometry[2],'RWGS',  temperature)
+rate_RWGS = rate_eq(C0, para_ethane, stoichiometry[2], 'RWGS', temperature)
 
-
-# Test on a reactor model to perform integration 
+# Test on a reactor model to perform integration
 reactor_ethane = Reactor(stoichiometry, tf, C0=C0, names=rxn_names, temperature=temperature)
 tC_profile = reactor_ethane.get_profile(rate_eq, para_ethane, t_eval=t_eval)
 # Plot the profile
 t_eval = tC_profile[:, 0]
 C_profile = tC_profile[:, 1:]
 plot_ethane(t_eval, C_profile, 'Ground truth', estimator_name)
-
 
 # Test the model bridge
 # Prase the specifics (reaction conditions) of reactor object into a dictionary
@@ -238,8 +239,8 @@ n_reactor = 1
 Y_experiments = [C_profile]
 
 # Set the weight for water as zero, others as 1 
-Y_weights = np.ones((n_reactor,m_specs))
-Y_weights[:,-1] = 0 
+Y_weights = np.ones((n_reactor, m_specs))
+Y_weights[:, -1] = 0
 
 # Input experimental data and models (rate expressions) into a model bridge
 bridge = ModelBridge(rate_eq, para_name_ethane, name=estimator_name)
@@ -256,22 +257,21 @@ t_vec_predict, Y_predict = bridge.profile(para_ground_truth)
 loss_ground_truth = bridge.loss_func(para_ground_truth)
 plot_ethane_overlap(t_vec_predict[0], Y_predict[0], t_eval, C_profile, 'Ground truth', estimator_name)
 
-
-# Set up an optimizer 
+# Set up an optimizer
 # Automatically compute the parameter ranges given a deviation
 deviation = 0.25
-para_ranges =  [[-np.abs(vi) *deviation + vi, np.abs(vi) *deviation + vi] for vi in para_ethane.values()]
+para_ranges = [[-np.abs(vi) * deviation + vi, np.abs(vi) * deviation + vi] for vi in para_ethane.values()]
 
 # Start a timer
 start_time = time.time()
 n_iter = 100
 optimizer = BOOptimizer(estimator_name)
 X_opt, loss_opt, Exp = optimizer.optimize(bridge.loss_func, para_ranges, n_iter, log_flag=True)
-end_time= time.time()
+end_time = time.time()
 
 # Predict the conversions given the optimal set of parameters
 t_opt, Y_opt = bridge.profile(X_opt)
-plot_ethane_overlap(t_opt[0], Y_opt[0], t_eval, C_profile,'Optimal Set', estimator_name)
+plot_ethane_overlap(t_opt[0], Y_opt[0], t_eval, C_profile, 'Optimal Set', estimator_name)
 
 # Check the left and right bounds without optimization
 para_left = [vi[0] for vi in para_ranges]
@@ -279,23 +279,22 @@ para_right = [vi[1] for vi in para_ranges]
 
 t_left, Y_left = bridge.profile(para_left)
 loss_left = bridge.loss_func(para_left)
-plot_ethane_overlap(t_left[0], Y_left[0],t_eval, C_profile, 'Left bound', estimator_name)
+plot_ethane_overlap(t_left[0], Y_left[0], t_eval, C_profile, 'Left bound', estimator_name)
 
 t_right, Y_right = bridge.profile(para_right)
 loss_right = bridge.loss_func(para_right)
-plot_ethane_overlap(t_right[0], Y_right[0], t_eval, C_profile,'Right bound', estimator_name)
-
+plot_ethane_overlap(t_right[0], Y_right[0], t_eval, C_profile, 'Right bound', estimator_name)
 
 # Print the results
 ut.write_results(estimator_name, start_time, end_time, loss_opt, X_opt)
 
-#%% Test on the ethane system, with noise
+# %% Test on the ethane system, with noise
 # Add to noise to the concentrations
 estimator_name = 'ethane_noisy'
 ut.clear_cache(estimator_name)
 
 noise_level = 0.00003
-noise_matrix = np.random.normal(loc=0,scale=noise_level, size=C_profile.shape)
+noise_matrix = np.random.normal(loc=0, scale=noise_level, size=C_profile.shape)
 C_profile_noisy = noise_matrix + C_profile
 plot_ethane_overlap(t_eval, C_profile_noisy, t_eval, C_profile, 'Noisy Data', estimator_name)
 
@@ -303,21 +302,20 @@ plot_ethane_overlap(t_eval, C_profile_noisy, t_eval, C_profile, 'Noisy Data', es
 Y_experiments_noisy = [C_profile_noisy]
 
 # Construct a ModelBridge object
-bridge_noisy = ModelBridge(rate_eq, para_name_ethane, name = estimator_name)
+bridge_noisy = ModelBridge(rate_eq, para_name_ethane, name=estimator_name)
 bridge_noisy.input_data(stoichiometry, reactor_data, Y_experiments_noisy, Y_weights, t_eval=t_eval, eval_profile=True)
 
 # Perform the optimization
 start_time = time.time()
 n_iter = 100
 optimizer_noisy = BOOptimizer(estimator_name)
-X_opt_noisy, loss_opt_noisy, Exp_noisy = optimizer_noisy.optimize(bridge_noisy.loss_func, para_ranges, n_iter, log_flag=True)
-end_time= time.time()
-
+X_opt_noisy, loss_opt_noisy, Exp_noisy = optimizer_noisy.optimize(bridge_noisy.loss_func, para_ranges, n_iter,
+                                                                  log_flag=True)
+end_time = time.time()
 
 # Predict the conversions given the optimal set of parameters
 t_opt_noisy, Y_opt_noisy = bridge_noisy.profile(X_opt_noisy)
 plot_ethane_overlap(t_opt_noisy[0], Y_opt_noisy[0], t_eval, C_profile, 'Optimal Set for noisy data', estimator_name)
-
 
 # Print the results
 ut.write_results(estimator_name, start_time, end_time, loss_opt, X_opt)
--- examples/ethane/use_prefactor_only/run_ethane_prefactor_only.py
+++ examples/ethane/use_prefactor_only/run_ethane_prefactor_only.py
@@ -4,7 +4,7 @@ Test on ethane system ODEs
 import os
 import sys
 
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 sys.path.insert(0, project_path)
 
 import time
