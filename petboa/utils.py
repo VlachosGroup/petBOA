08ff8eaa5b1fd65f085a246d18ddd3e0649bf504
Sashank Kasiraju <skasiraj@udel.edu>
2023-10-26 11:23:54 -0400

    Updating the utils.py module to parse parameter input files

--- petboa/utils.py
+++ petboa/utils.py
@@ -4,7 +4,8 @@ Utility functions
 import os
 import numpy as np
 import shutil
-
+import pandas as pd
+from pmutt.io.excel import read_excel
 
 # Loss functions
 # reference: https://ml-cheatsheet.readthedocs.io/en/latest/loss_functions.html
@@ -47,3 +48,45 @@ def para_values_to_dict(xi, para_names):
     for name_i, para_i in zip(para_names, xi):
         para_dict.update({name_i: para_i})
     return para_dict
+
+
+def parse_param_file(file_path):
+        try:
+            # Attempt to read as an Excel file
+            excel_data = read_excel(file_path,
+                                    skiprows=0,
+                                    header=0,
+                                    )
+            bounds = get_param_bounds(excel_data)
+            names = get_param_names(excel_data)
+        except ValueError:
+            try:
+                # Attempt to read as a CSV file
+                csv_data = pd.read_csv(file_path)
+                print(csv_data)
+                bounds = get_param_bounds(csv_data)
+                names = get_param_names(csv_data)
+            except pd.errors.EmptyDataError:
+                return None  # The file is empty or could not be read
+        return names, bounds
+
+def get_param_bounds(data):
+    bounds = []
+    for r in data:
+        try:
+            if r["Select"] is True:
+                bounds.append([r['LB'], r['UB']])
+            else:
+                bounds.append([r['Default']])
+        except:
+            return None
+    return bounds
+
+def get_param_names(data):
+    names = []
+    for r in data:
+        try:
+            names.append(r["Name"])
+        except:
+            return None
+    return names
